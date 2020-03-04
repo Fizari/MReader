bc888316fe8252c69451232994687ccdf8022c3d
Vincent Fauveau <fauveauvincent@gmail.com>
2020-03-04 14:30:29 +0100

    Initial commit with MVVM pattern and main view.

---
+++ MReader.Core/Converters/ImagePanelWidthConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+
+namespace MReader.Core.Converters
+{
+    public class ImagePanelWidthConverter : IMultiValueConverter
+    {
+        //value[0] should be the grid width in DOUBLE
+        //value[1] should be splitter width in DOUBLE
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values[0] == DependencyProperty.UnsetValue)
+            {
+                return 100000;
+            }
+            double res_width = System.Convert.ToDouble(values[0]) - (System.Convert.ToDouble(values[1]) * 2);
+            return res_width;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException("Cannot convert back");
+        }
+    }
+}
---
+++ MReader.Core/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,52 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+
+namespace MReader.Core.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string _title = "MReader - Dev";
+
+        private int _splittersWidth = 10;
+        private int _imagePanelMinWidth = 100;
+
+        private DelegateCommand _pressme;
+        public DelegateCommand PressMe =>
+            _pressme ?? (_pressme = new DelegateCommand(ExecuteCommandName));
+
+        void ExecuteCommandName()
+        {
+            //SplittersWidth == 10 ? SplittersWidth = 50 : SplittersWidth = 10;
+            if (SplittersWidth == 10)
+            {
+                SplittersWidth = 50;
+            }
+            else
+            {
+                SplittersWidth = 10;
+            }
+        }
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        public int SplittersWidth 
+        {
+            get { return _splittersWidth; }
+            set { SetProperty(ref _splittersWidth, value); }
+        }
+
+        public int ImagePanelMinWidth
+        {
+            get { return _imagePanelMinWidth; }
+            set { SetProperty(ref _imagePanelMinWidth, value); }
+        }
+
+        public MainWindowViewModel()
+        {
+
+        }
+    }
+}
